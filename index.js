a5bd9412d93cfbdf4795d0261b0068292c1d860f
Elias Sierra <elfleat@Eliass-MBP.attlocal.net>
2018-11-11 19:58:02 -0500

    godaddy api

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const request = require('request')
+const whois = require('whois-json')
 const app = express()
 
 const port = process.env.PORT || 3001
@@ -9,22 +11,63 @@ app.get('/wake-up', (req, res) => {
     })
 })
 
+// app.get('/checkv2/:domain', async (req, res) => {    
+//     const { domain } = req.params
+//     const whois = require('whois-json')
+
+//     console.log(`Domain requested: ${domain}`)
+//     try {
+//         const result = await whois(domain, {follow: 3, verbose: true});
+//         const isTaken = result && result.length > 1 ? true : false;
+        
+//         console.log(`${domain} is ${isTaken ? '' : 'not'} taken`)
+    
+//         res.json({
+//             domain,
+//             isTaken,
+//             success: true
+//         })
+
+//     } catch(error) {
+//         console.log(`Request failed for: ${domain}`)
+
+//         res.json({
+//             success: false,
+//             error
+//         })
+//     }
+// })
+
+
 app.get('/check/:domain', async (req, res) => {    
     const { domain } = req.params
-    const whois = require('whois-json')
-
+    let isTaken
     console.log(`Domain requested: ${domain}`)
     try {
-        const result = await whois(domain, {follow: 3, verbose: true});
-        const isTaken = result && result.length > 1 ? true : false;
+
+        var headers = {
+            'accept': 'application/json',
+            'Authorization': 'sso-key 9EE5Efgwg99_QdqnHVHsfmzz7bPbDQ2Wkb:QdqpvkDqp813qEhurZrtEf'
+        };
         
-        console.log(`${domain} is ${isTaken ? '' : 'not'} taken`)
-    
-        res.json({
-            domain,
-            isTaken,
-            success: true
-        })
+        var options = {
+            url: `https://api.godaddy.com/v1/domains/available?domain=${domain}&checkType=FAST&forTransfer=false`,
+            headers: headers
+        };
+        
+        request(options, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                body = JSON.parse(body)
+                // console.log(body)
+                const isTaken = !body.available
+                console.log(`${domain} is ${isTaken ? '' : 'not'} taken`)
+                res.json({
+                    domain,
+                    isTaken,
+                    success: true
+                })
+            }
+        });
 
     } catch(error) {
         console.log(`Request failed for: ${domain}`)
